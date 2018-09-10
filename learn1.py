72f2e3332a77d4139d55f2749134228786c8cf12
Zhong_wj <897915564@qq.com>
2018-09-10 16:11:05 +0800

    gogos

--- learn1.py
+++ learn1.py
@@ -1,6 +1,17 @@
+# coding:utf-8
+import urllib.request
+import re
 
-from urllib.request import urlopen
+def get_html(url):
+    page = urllib.request.urlopen(url)
+    html = page.read()
+    html = html.decode('utf-8')  # python3
+    return html
 
-html = urlopen('https://jr.jd.com')
-print(html.read())
-html.close()
+reg = r'src="(.+?\.jpg)" width'#正则表达式
+reg_img = re.compile(reg)#编译一下，运行更快
+imglist = reg_img.findall(get_html('http://tieba.baidu.com/p/1753935195'))#进行匹配
+x =0
+for img in imglist:
+    urllib.request.urlretrieve(img,'%s.jpg' %x)
+    x += 1
