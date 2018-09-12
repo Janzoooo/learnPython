e797137474608f5e3903d63d0b4c6fd98147d53e
Zhong_wj <897915564@qq.com>
2018-09-12 16:45:36 +0800

    first demo

--- learn1.py
+++ learn1.py
@@ -1,4 +1,5 @@
 # coding:utf-8
+'''
 import urllib.request
 import re
 
@@ -15,3 +16,70 @@ x =0
 for img in imglist:
     urllib.request.urlretrieve(img,'%s.jpg' %x)
     x += 1
+
+'''
+
+
+'''
+#example
+from bs4 import BeautifulSoup
+html_sample= ' \
+<html> \
+    <body> \
+    <h1 id="title">Hello Word</h1> \
+    <a href="#" class="link">This is link1</a> \
+    <a href="# link2" class="link">This is link2</a> \
+    </body> \
+    </html>'
+soup = BeautifulSoup(html_sample, 'html.parser')
+print(type(soup))
+print(soup.text)
+
+print('---------')
+
+header = soup.select('h1')
+print(header[0])
+print(header[0].text)
+
+print('----------')
+
+alink = soup.select('#title')
+print(alink[0])
+
+print('----------')
+
+for link in soup.select('.link'):
+    print(type(link))
+    print(link['href'])
+
+print('----------')
+
+'''
+import csv
+import requests
+from bs4 import BeautifulSoup
+
+res = requests.get('https://news.sina.com.cn/china/')
+res.encoding = 'utf-8'
+soup = BeautifulSoup(res.text, 'html.parser')
+news = soup.select('.right-content')[0]
+
+#创建csv文件准备导入
+csvfile = open('news.csv', 'w',newline='')
+try:
+    writer = csv.writer(csvfile)
+    writer.writerow(['时间','标题','网址'])
+
+
+    for a_link in news.select('a'):
+        new_title = a_link.text
+        new_url = a_link['href']
+        res_click = requests.get(new_url)
+        res_click.encoding = 'utf-8'
+        soup_click = BeautifulSoup(res_click.text, 'html.parser')
+        date = soup_click.select('.date')[0].text
+        #print(date, new_title, new_url)
+        writer.writerow([date, new_title, new_url])
+
+finally:
+    csvfile.close()
---
+++ test_Selenium+Headless_Chrome.py
@@ -0,0 +1,38 @@
+import csv
+from selenium import webdriver
+option = webdriver.ChromeOptions()
+option.add_argument("headless")
+option.add_argument('UTF-8')
+driver = webdriver.Chrome("C:\Python27\Scripts\chromedriver.exe", chrome_options=option)
+driver_in = webdriver.Chrome("C:\Python27\Scripts\chromedriver.exe", chrome_options=option)
+print('打开新浪新闻首页')
+driver.get("https://news.sina.com.cn/china/")
+news_list = driver.find_elements_by_css_selector('div#feedCardContent>div>div.feed-card-item')
+print('创建csv文件准备导入')
+csvfile = open('hotnews.csv', 'w', newline='')
+csvwriter = csv.writer(csvfile)
+csvwriter.writerow(['时间','标题','网址','评论数'])
+print('查询写入中...')
+num = 1
+for news in news_list:
+    news_title = news.find_elements_by_css_selector('h2>a')[0].text
+    news_url = news.find_elements_by_css_selector('h2>a')[0].get_attribute('href')
+    news_date = news.find_element_by_class_name('feed-card-time').text
+    driver_in.get(news_url)
+    #print(news_url)
+    #print(driver.find_elements_by_css_selector('div.page-tools>span.tool-cmt>a>span.num')[0].text == '')
+    news_comments_num = '0' if driver_in.\
+                                   find_elements_by_css_selector('div.page-tools>span.tool-cmt>a>span.num')\
+                                   [0].text == '' else\
+            driver_in.find_elements_by_css_selector('div.page-tools>span.tool-cmt>a>span.num')[0].text
+
+    csvwriter.writerow([news_date, news_title, news_url, news_comments_num])
+
+    print(num)
+    num = num + 1
+
+print('关闭csv文件')
+#csvfile.close()
+print('关闭浏览器')
+driver.quit()
+driver_in.quit()
